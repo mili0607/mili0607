d35ba8fd92f204f49f95ae362a97c4a433f1dbae
mili0607 <156015057+mili0607@users.noreply.github.com>
2024-01-10 13:45:41 -0600

    lll
    primer proyecto de clase

---
+++ primer proyecto/primer proyecto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="primer_proyecto.Global" Language="C#" %>
---
+++ primer proyecto/primer proyecto/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(primer_proyecto.Startup))]
+namespace primer_proyecto
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
