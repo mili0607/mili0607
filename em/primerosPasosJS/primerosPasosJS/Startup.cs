ff87912427a2b008e3e88381af6251372417de51
mili0607 <emily06rs@gmail.com>
2024-01-15 13:46:12 -0600

    proyec2
    proyec 2

---
+++ em/primerosPasosJS/primerosPasosJS/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(primerosPasosJS.Startup))]
+namespace primerosPasosJS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
