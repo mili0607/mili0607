1bc293101b22693180f22da56bf7c6555274709c
mili0607 <emily06rs@gmail.com>
2024-01-17 13:59:28 -0600

    banco
    banco

---
+++ practica JS 2/practica JS 2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(practica_JS_2.Startup))]
+namespace practica_JS_2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
