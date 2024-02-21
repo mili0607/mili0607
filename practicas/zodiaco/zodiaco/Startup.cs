99674ebaf112b823458110fb5faa8458b4198b6f
mili0607 <emily06rs@gmail.com>
2024-02-21 13:54:08 -0600

    ZODI

---
+++ practicas/zodiaco/zodiaco/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(zodiaco.Startup))]
+namespace zodiaco
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
