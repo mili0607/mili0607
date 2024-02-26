cb3f1bc3cfbe80bd7b13839f930458f5175668b3
mili0607 <emily06rs@gmail.com>
2024-02-26 12:17:56 -0600

    tare
    tare

---
+++ clase 26-02-24/clase 26-02-24/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="clase_26_02_24.Global" Language="C#" %>
---
+++ clase 26-02-24/clase 26-02-24/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(clase_26_02_24.Startup))]
+namespace clase_26_02_24
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
